8ceca3e89e00bb295b6291b3f4866bae1a1083e6
Yiim <ilyimyan@gmail.com>
2023-08-19 13:43:00 +0800

    initial commit

--- script.js
+++ script.js
@@ -31,9 +31,11 @@ console.log('');
 
 */
 
-for(let i = 0 ; i < 10 ; i++) {
-    for(let j = 0 ; j < i ; j--) {
+for(let i = 10 ; i < 0 ; i--) {
+    for(let j = 0 ; j < i ; j++) {
         string += '*';
     }
     string += '\n'
 }
+
+console.log(string)
