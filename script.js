42dd0bf4f9e7da824319f846041b60d3107258b6
Yiim <ilyimyan@gmail.com>
2023-08-19 13:10:18 +0800

    initial commit

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 // result 
 
+/* 
+*
+**
+***
+****
+*****
+******
+*******
+********
+*********
+**********
+*/
+
 let int = 10;
 let str = '*'
 let emptyString = '';
 
 for(let i = 0 ; i < int ; i++) {
     console.log(emptyString += str);
+}
+
+console.log('')
+
+let int2 = 0;
+let str2 = '*';
+let emptyString2 = '';
+
+for(let i = 10 ; i > int2 ; i--) {
+    console.log(emptyString2 += str2)
 }
